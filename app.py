51108823070e1d928347e7b77faf07840de5c5fb
Sambit Pritam <65372291+sambit-0007@users.noreply.github.com>
2024-11-12 02:10:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ import pickle
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+import logging
+
 # load the lstm model 
+logging.basicConfig(level=logging.DEBUG)
 model = load_model("next_word_lstm.h5")
 
 # Load the tokenizer 
